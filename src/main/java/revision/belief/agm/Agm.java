9802a65fb02825e47624615adadddd8a6b0c88e9
Musti :) <70944527+WettyWednesday@users.noreply.github.com>
2025-04-29 09:03:41 +0200

    started work on postulate methods

--- src/main/java/revision/belief/agm/Agm.java
+++ src/main/java/revision/belief/agm/Agm.java
@@ -1,6 +1,22 @@
 package revision.belief.agm;
 
+import revision.belief.beliefbase.BeliefBase;
+import revision.belief.logic.Formula;
+import revision.belief.revision.Revision;
+
 public class Agm {
-    
+
+    // Success P includes K revised P
+    public boolean satisfySuccess(BeliefBase base, Formula formula, int priority) {
+        if (!base.contains(formula)) {
+            Revision.revise(base, formula, priority);
+            return base.contains(formula);
+        }
+        return false;
+    }
+
+    public boolean satisfyInclusion(BeliefBase base, Formula formula, int priority) {
+
+    }
 
 }
